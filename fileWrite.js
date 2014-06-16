6478b5d1eed1b47805fc249cf88003fa9b3cd51f
scgarland191 <scgarland191@yahoo.com>
2014-06-16 10:49:30 -0400

    Alter fileWrite.js

--- fileWrite.js
+++ fileWrite.js
@@ -3,8 +3,7 @@ var myFile = 'newFile.txt'
 var text = 'Hello World!\n'
 fs.appendFile(myFile, text, function (err) {
 	if (err){
-		console.log('Something has gone wrong!');
-		throw err;
+		console.log('Something has gone wrong!\n Error: "+err);
 	}
-	console.log('Hello World! has been added');
+	console.log(myFile + ' has been added');
 });
